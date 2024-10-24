068ca2e8ec84bcc9b50280d9dd496e78cc8a4354
AMARDEEP KUMAR <GTMAMAR22@GMAIL.COM>
2024-10-24 23:18:54 +0530

    Deleting files which were placed in wrong folder

--- FindOldCartsDao.java
+++
@@ -1,8 +0,0 @@
-package org.training.core.daos;
-
-import de.hybris.platform.core.model.order.CartModel;
-import de.hybris.platform.servicelayer.search.SearchResult;
-
-public interface FindOldCartsDao {
-    public SearchResult<CartModel> findOldCarts(int hours);
-}
